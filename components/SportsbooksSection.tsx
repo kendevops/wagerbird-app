c5bd8b38c9ee7c4e7ffbc15d68d5d98054f19d04
Kenneth Aladi <kendevops@gmail.com>
2026-03-13 13:39:22 +0100

    updated the sportsbooks reward showing or hiding

--- components/SportsbooksSection.tsx
+++ components/SportsbooksSection.tsx
@@ -131,7 +131,7 @@ export default function SportsbooksSection({
               }
               onClick={() => setActiveCategory("sportsbook")}
             >
-              SPORTBOOKS
+              SPORTSBOOKS
             </button>
             <button
               type="button"
